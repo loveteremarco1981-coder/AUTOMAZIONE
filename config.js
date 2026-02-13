ce57a7d73bf28172a05cc21829a221fd20135d34
loveteremarco1981-coder <lovetere.marco1981@gmail.com>
2026-02-13 23:18:52 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-<script>
-  window.APP_CONFIG = {
-    endpoint: "https://script.google.com/macros/s/AKfycbzWadQvolbaH1hIUFHjcSXsMcH9Z9iTss1Ty-uiVVKQ-TjMOBfajv3e262tnH_1Th0A/exec"
-  };
-</script>
+window.APP_CONFIG = {
+  endpoint: "https://script.google.com/macros/s/AKfycbzWadQvolbaH1hIUFHjcSXsMcH9Z9iTss1Ty-uiVVKQ-TjMOBfajv3e262tnH_1Th0A/exec"
+};
