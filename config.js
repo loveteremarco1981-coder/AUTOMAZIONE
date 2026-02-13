72032b8f3b91b5aa83dc965e1147d6fd0fed3f21
loveteremarco1981-coder <lovetere.marco1981@gmail.com>
2026-02-13 22:36:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
-window.APP_CONFIG = { endpoint: 'https://script.google.com/macros/s/AKfycbzWadQvolbaH1hIUFHjcSXsMcH9Z9iTss1Ty-uiVVKQ-TjMOBfajv3e262tnH_1Th0A/exec' };
+<script>
+  window.APP_CONFIG = {
+    // INCOLLA QUI il tuo URL /exec della Web App GAS
+    endpoint: "https://script.google.com/macros/s/AKfycbzWadQvolbaH1hIUFHjcSXsMcH9Z9iTss1Ty-uiVVKQ-TjMOBfajv3e262tnH_1Th0A/exec"
+  };
+</script>
