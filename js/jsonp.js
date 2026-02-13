4ebc22e5a41f11b843dd07cb174590c1483c38df
loveteremarco1981-coder <lovetere.marco1981@gmail.com>
2026-02-13 22:36:54 +0100

    Update jsonp.js

--- js/jsonp.js
+++ js/jsonp.js
@@ -1,4 +1,3 @@
-
 function jsonp(url, callbackParam='callback'){
   return new Promise((resolve, reject)=>{
     const cb='cb_'+Math.random().toString(36).slice(2);
@@ -6,7 +5,7 @@ function jsonp(url, callbackParam='callback'){
     const s=document.createElement('script');
     s.src = `${url}${sep}${callbackParam}=${cb}`;
     window[cb] = (data)=>{ resolve(data); s.remove(); delete window[cb]; };
-    s.onerror = (e)=>{ reject(e); s.remove(); delete window[cb]; };
+    s.onerror = ()=>{ reject(new Error('JSONP error')); s.remove(); delete window[cb]; };
     document.head.appendChild(s);
   });
 }
