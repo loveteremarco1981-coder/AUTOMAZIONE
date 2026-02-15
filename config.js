709ec8b2b19f4e7434345aa36e4d676013c5a0b9
loveteremarco1981-coder <lovetere.marco1981@gmail.com>
2026-02-15 23:12:12 +0100

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-// Imposta qui il tuo Web App /exec e i parametri base della dashboard
-window.DASH = {
-  WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbzx-jta1hBn-PZmXj7IGhpO_B2uNAz_G5BrGaQ6V7lZYEf2VBXGHCyr_ho8Xlo7jNEj/exec',
-  REFRESH_MS: 60_000,     // refresh automatico ogni 60s
-  UNIT_KEY:   'dash.unit' // localStorage key per °C/°F
-};
