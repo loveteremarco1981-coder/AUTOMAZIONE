ee0498a37c854114399bcd239f7c880ccd1f92d7
loveteremarco1981-coder <lovetere.marco1981@gmail.com>
2026-02-17 14:24:18 +0100

    Add files via upload

---
+++ icons/README.txt
@@ -0,0 +1,8 @@
+Sostituisci questi PNG con esportazioni dal tuo SVG "casetta" (link Vecteezy)
+
+Consigliato:
+- house-192.png (192x192)
+- house-512.png (512x512)
+- house-maskable-192.png (192x192, con safe area)
+- house-maskable-512.png (512x512, con safe area)
+- apple-touch-icon-180.png (180x180, sfondo pieno o trasparente)
---
+++ sw.js
@@ -0,0 +1,33 @@
+// SW minimale: cache statici di base
+const CACHE = 'casa-static-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './config.js',
+  './jsonp.js',
+  './app.js',
+  './manifest.webmanifest',
+  './icons/house-192.png',
+  './icons/house-512.png',
+  './icons/house-maskable-192.png',
+  './icons/house-maskable-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
